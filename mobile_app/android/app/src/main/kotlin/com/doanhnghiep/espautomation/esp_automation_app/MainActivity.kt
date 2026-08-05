0ae6d4e73a912f25b1fe04d6cf77dd4c8277538e
Long Louis <thanhlongts2k@gmail.com>
2026-08-05 17:00:31 +0700

    feat: initialize Flutter Android build files, dependency config, and main app entrypoint

---
+++ mobile_app/android/app/src/main/kotlin/com/doanhnghiep/espautomation/esp_automation_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.doanhnghiep.espautomation.esp_automation_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -60,7 +60,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   @override
   void initState() {
-    super.initState() ;
+    super.initState();
     _initMqtt();
   }
 
